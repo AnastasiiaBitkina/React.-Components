bd35d9c7d40459a1b4eabb53f924ff75c4afc285
anastasia <anastasia.ethno@gmail.com>
2023-10-21 23:47:14 +0200

    feat: add input to Header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import Input from './Input';
+import '../Styles/header.css';
+
+function Header() {
+    return (
+        <Input />
+    )
+}
+
+export default Header;
---
+++ src/Components/Input.tsx
@@ -0,0 +1,21 @@
+import React, { useState, ChangeEvent } from 'react';
+import '../Styles/input.css'
+
+const Input: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <input
+      type="text"
+      value={inputValue}
+      onChange={handleInputChange}
+      placeholder="Enter breed"
+    />
+  );
+};
+
+export default Input;
