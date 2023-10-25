0fe7d7636391dcf04643c86e0cb62802773a0fbf
anastasia <anastasia.ethno@gmail.com>
2023-10-25 21:26:15 +0200

    feat: add external api

--- src/Components/DogCard.tsx
+++ src/Components/DogCard.tsx
@@ -1,12 +1,24 @@
 import { FunctionComponent } from "react";
+//import { useEffect, useState } from 'react';
+import ApiComponent from './ApiComponent';
 
 interface Props {
     breed: string;
 }
-export const DogCard: FunctionComponent<Props> = ({breed}) => {
+
+export const DogCard: FunctionComponent<Props> = () => {
+   /* const [randomBreed, setRandomBreed] = useState<string | null>('');
+
+    useEffect(() => {
+
+        const breeds = ApiComponent.getDataFromApi(); 
+        const randomIndex = Math.floor(Math.random() * breeds.length);
+        setRandomBreed(breeds[randomIndex]);
+    }, []);*/
+
     return (
         <div>
-        <p>{breed || 'Unknown breed'}</p>
+        <p>{<ApiComponent /> || 'Unknown breed'}</p>
         </div>  
     );
 }
