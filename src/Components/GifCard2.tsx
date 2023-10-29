98217c86d804ad6a9d98e59294f9273ec2add172
anastasia <anastasia.ethno@gmail.com>
2023-10-29 08:47:22 +0100

    fix: add function for mix gifs

--- src/Components/GifCard2.tsx
+++ src/Components/GifCard2.tsx
@@ -2,39 +2,21 @@ import { useEffect, useState } from "react";
 import Pagination from './Pagination';
 import { getDataFromApi, GifData } from './apiFunctions';
 
-/*interface GifData {
-    data: {
-        images: {
-            original: {
-                url: string;
-            };
-        };
-    }[];
-}*/
 
-//export const GifCard2 = () => {
-    //const [gifs, setData] = useState<GifData>({ data: [] });
 
-    export const GifCard2: React.FC = () => {
-        const [gifs, setData] = useState<GifData>({ data: [] });
+export const GifCard2: React.FC = () => {
+    const [gifs, setData] = useState<GifData>({ data: [] });
 
     useEffect(() => {
         console.log("hi");
         getDataFromApi().then((data: GifData) => {
-          console.log(1);
-          setData(data);
-        }).catch((error) => {
-          console.error('Error loading data:', error);
-        });
-      }, []);
-
-   /* useEffect(() => {
-        console.log("hi")
-        getDataFromApi().then(data => {
             console.log(1);
             setData(data);
-        })
-    }, []);*/
+        }).catch((error) => {
+            console.error('Error loading data:', error);
+        });
+    }, []);
+
 
     return (
         <div>
@@ -42,20 +24,6 @@ import { getDataFromApi, GifData } from './apiFunctions';
         </div>
     );
 
-    /*return (
-        <div>
-            <h2>Random Gif</h2>
-            <ul>
-                {gifs.data.map((gif, index) => {
-                    return (<div key={index}>
-                      <h3>Title</h3>
-                    <img src= {gif.images.original.url} alt="" />  
-                    </div>)
-                })
-                }
-
-            </ul>
-        </div>
-            )*/};
+};
 
 export default GifCard2;
--- src/Components/Pagination.tsx
+++ src/Components/Pagination.tsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { GifData } from "./apiFunctions";
 
 interface PaginationProps {
   gifs: GifData;
 }
 
+interface ShuffledGifs {
+  data: GifData["data"];
+}
+
 const Pagination: React.FC<PaginationProps> = ({ gifs }) => {
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [shuffledGifs, setShuffledGifs] = useState<ShuffledGifs | null>(null);
   const itemsPerPage: number = 5;
 
+  useEffect(() => {
+    if (gifs.data) {
+      const shuffled = shuffleArray(gifs.data);
+      setShuffledGifs({ data: shuffled });
+    }
+  }, [gifs]);
+
   const displayGifs = () => {
+    if (!shuffledGifs) {
+      return null;
+    }
+
     const startIndex: number = (currentPage - 1) * itemsPerPage;
     const endIndex: number = currentPage * itemsPerPage;
-    const currentGifs = gifs.data.slice(startIndex, endIndex);
+    const currentGifs = shuffledGifs.data.slice(startIndex, endIndex);
 
     return (
       <div>
@@ -41,6 +57,15 @@ const Pagination: React.FC<PaginationProps> = ({ gifs }) => {
     }
   };
 
+  const shuffleArray = (array: GifData["data"]) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  };
+
   return (
     <div>
       {displayGifs()}
@@ -60,4 +85,3 @@ const Pagination: React.FC<PaginationProps> = ({ gifs }) => {
 };
 
 export default Pagination;
-
